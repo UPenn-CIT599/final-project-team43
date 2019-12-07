60032a5af2750e59f7bd7d5e49238b73a077801a
Sally <songqi26@outlook.com>
2019-12-07 17:53:15 -0500

    add comments

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,15 +1,14 @@
 
 /**
+ *This class is the start of the program
  *
  * @author Songqi(Sally) Li
  */
 public class Runner {
 
   public static void main(String[] args) {
-    java.awt.EventQueue.invokeLater(new Runnable() {
-      public void run() {
-        new LifeExpectancyForm().setVisible(true);
-      }
+    java.awt.EventQueue.invokeLater(() -> {
+      new LifeExpectancyForm().setVisible(true);
     });
   }
 }
