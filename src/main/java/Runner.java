29d4c51a7f31fe894567700560a035def862db2b
Sally <songqi26@outlook.com>
2019-12-08 00:50:23 -0500

    Add comments on the code

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,8 @@
 
 /**
- *This class is the start of the program
+ *This class is the start of the program.
+ * The program will calculate the minimum interest rate for a working
+ * Canadian (18-64 age) to achieve target retirement savings plan at retirement age.
  *
  * @author Songqi(Sally) Li
  */
